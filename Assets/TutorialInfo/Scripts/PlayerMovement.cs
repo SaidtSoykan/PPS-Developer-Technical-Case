337f0689bb852be73572e7fa07f6e01837059208
Saidt <saidsoykan@outlook.com>
2022-08-22 17:27:36 +0300

    Player Movement Script Added

--- Assets/TutorialInfo/Scripts/PlayerMovement.cs
+++ Assets/TutorialInfo/Scripts/PlayerMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //To be able to edit the walking speed from the editor
+    public float walkSpeed;
+    public float mouseSensitive;
+
+    public GameObject cam;
+    public GameObject caps;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        playerMove();
+        camMove();
+    }
+    void playerMove()
+    {
+        //got key input from user
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
+        //to fix the extra speed in the diagonal movement
+        Vector3 way = Vector3.Normalize(new Vector3(-x, 0, z));
+        //to move the player according to the key pressed
+        transform.Translate(way * walkSpeed * Time.deltaTime);
+    }
+    void camMove()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+        caps.transform.Rotate(new Vector3(0, 1, 0) * x * mouseSensitive);
+        cam.transform.Rotate(new Vector3(-1, 0, 0) * y * mouseSensitive);
     }
 }
